3b601cd6a4d5b38b830bd88cb1507a0fd648141e
francescozoccheddu <zoccheddu.fra@gmail.com>
2017-10-22 13:35:58 +0200

    json batch

--- afileio.cpp
+++ afileio.cpp
@@ -9,7 +9,6 @@ AFileIO::AFileIO()
 QString AFileIO::read(QString filename)
 {
     QFile file(filename);
-    file.setFileName("C:/Users/zocch/Desktop/test.json");
     file.open(QIODevice::ReadOnly | QIODevice::Text);
     QString content = file.readAll();
     file.close();
