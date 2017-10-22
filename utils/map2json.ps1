95de0998c929dc300c783617d5f817320b2a2146
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-10-22 13:35:58 +0200

    json batch

--- utils/map2json.ps1
+++
@@ -1,7 +0,0 @@
-Param(
-    [string]$projdir="C:\Users\zocch\Onedrive\Unica\ARE",
-    [string]$projname="ARE",
-    [string]$scriptfile=".\map2json.py"
-)
-$sources = Get-ChildItem "$projdir\$projname\*.asm"
-python $scriptfile "$projdir\$projname\Debug\$projname.map" "$projdir\$projname\Debug\$projname.eep" $sources
