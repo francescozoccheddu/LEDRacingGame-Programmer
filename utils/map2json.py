dd56d57580c37fd304d85075f6c377521c1325e0
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-10-22 09:30:44 +0200

    m2j untested alpha

--- utils/map2json.py
+++ utils/map2json.py
@@ -1,15 +1,31 @@
 import sys
 import argparse
 import re
+import json
+import intelhex
 
-re_search_map_eseg = re.compile(r"ESEG\s*(?P<name>[0-9A-Za-z][0-9A-Za-z_]*)\s*(?P<address>[0-9A-Fa-f]*)")
-reb_match_source = r';\s*name="(?P<name>[^\n]*)"\s*(;\s*description="(?P<description>[^\n]*)"\s*)?;\s*type="(?P<type>[^\n]*)"\s*%s:.*$'
+jsonName = "name"
+jsonDescription = "description"
+jsonType = "type"
+jsonAddress = "address"
+jsonSize = "size"
+jsonData = "data"
+jsonDefaultValue = "defvalue"
+
+re_search_map_eseg = re.compile(r"ESEG\s*(?P<label>[0-9A-z]\w*)\s*(?P<%s>[0-9A-Fa-f]*)" % jsonAddress) 
+_reb_search_source_name = r"\n[^\S\n]*;[^\S\n]*name=\"(?P<name>[^\n\"]*)\"[^\S\n]*"
+_reb_search_source_descr = r"\n[^\S\n]*;[^\S\n]*description=\"(?P<description>[^\n\"]*)\"[^\S\n]*"
+_reb_search_source_type = r"\n[^\S\n]*;[^\S\n]*type=\"(?P<type>[^\n\"]*)\"[^\S\n]*"
+_reb_search_source_size = r"\n[^\S\n]*;[^\S\n]*size=(?P<size>\d+)[^\S\n]*"
+_reb_search_source_data = r"\n[^\S\n]*;[^\S\n]*data=(?P<data>{[^\n{}]*})[^\S\n]*"
+_reb_search_source_label = r"\n[^\S\n]*%s:"
+reb_search_source = _reb_search_source_name + _reb_search_source_descr + _reb_search_source_type + _reb_search_source_size + _reb_search_source_data + _reb_search_source_label
 
 def parseArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("map", metavar="<MAP_FILE>", type=argparse.FileType('r'), help="map file")
-    parser.add_argument("hex", metavar="<EEPROM_HEX_FILE>", type=argparse.FileType('r'), help="eeprom hex file")
-    parser.add_argument("sources", metavar="<SOURCE_FILE>", type=argparse.FileType('r'), help="source files", nargs="*")
+    parser.add_argument("mapfile", metavar="<MAP_FILE>", type=argparse.FileType('r'), help="map file")
+    parser.add_argument("hexfile", metavar="<EEPROM_HEX_FILE>", type=argparse.FileType('r'), help="eeprom hex file")
+    parser.add_argument("sourcefiles", metavar="<SOURCE_FILE>", type=argparse.FileType('r'), help="source files", nargs="*")
     parser.add_argument("-o", "--out", type=argparse.FileType('w'), default=sys.stdout, help="output file")
     return parser.parse_args()
 
@@ -18,39 +34,60 @@ def parseFromMap(mapfile):
     for line in mapfile:
         res = re_search_map_eseg.search(line)
         if res is not None:
-            name = res.group("name")
-            address = int(res.group("address"), 16)
-            parameter = Parameter(name)
-            parameter.dict["address"] = address
+            label = res.group("label")
+            address = int(res.group(jsonAddress), 16)
+            parameter = Parameter(label)
+            parameter.dict[jsonAddress] = address
             parameters += [parameter]
     return parameters
 
 def parseFromSource(source, parameters):
     for parameter in parameters:
-        reb_match_source_inst = reb_match_source % parameter.name
+        reb_match_source_inst = reb_search_source % parameter.label
         res = re.search(reb_match_source_inst, source, re.MULTILINE)
         if res is not None:
-            print(reb_match_source_inst)
-            print(res)
+            parameter.dict[jsonName] = res.group(jsonName)
+            parameter.dict[jsonDescription] = res.group(jsonDescription)
+            parameter.dict[jsonType] = res.group(jsonType)
+            parameter.dict[jsonSize] = int(res.group(jsonSize))
+            parameter.dict[jsonData] = json.loads(res.group(jsonData))
+        else:
+            raise RuntimeError("No description found for label '%s'" % parameter.label)
 
+def parseFromHex(hexfile, parameters):
+    hexdict = intelhex.IntelHex(hexfile).todict()
+    for parameter in parameters:
+        vals = []
+        for offset in (0, parameter.dict[jsonSize]):
+            vals += [hexdict[parameter.dict[jsonAddress] + offset]]
+        parameter.dict[jsonDefaultValue] = vals
 
 class Parameter:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, label):
+        self.label = label
         self.dict = {}
 
 def main():
     args = parseArgs()
     
-    parameters = parseFromMap(args.map)
-    args.map.close()
+    parameters = parseFromMap(args.mapfile)
+    args.mapfile.close()
 
     source = ""
-    for sourcefile in args.sources:
+    for sourcefile in args.sourcefiles:
         source += sourcefile.read()
         sourcefile.close()
 
     parseFromSource(source, parameters)
+
+    parseFromHex(args.hexfile, parameters)
+
+    dicts = []
+    for parameter in parameters:
+        dicts += [parameter.dict]
+    
+    out = json.dumps(dicts)
+    print(out)
 
     return
 
